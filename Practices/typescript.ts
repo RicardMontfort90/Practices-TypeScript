d591205d8079be8a5ce5f6d25b56a3a61e8c2bdd
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-11-14 18:25:41 +0100

    pruebas

--- Practices/typescript.ts
+++ Practices/typescript.ts
@@ -2,14 +2,14 @@
 //como a y b infiere que son number sin decirle nada
 const a = 1
 const b = 2
-
 const c = a + b
+// c también será number
 
-let n = 'hola'
+let cadenadetexto = 'hola'
 
-n.toLocaleLowerCase()
+// ❌cadenadetext.toLocaleLowerCase()
+// ❌cadenatexto.propiedadInexistente
 
-n = 2
 // any
 let obj: any = { x: 0 };
 
@@ -18,3 +18,26 @@ obj();
 obj.bar = 100;
 obj = "hello";
 const n: number = obj;
+
+// functions 
+//function saludar(name: string) {
+//    console.log(`Hola ${name}`)
+//}
+
+//✅ saludar('Manu')
+//❌saludar(2) Argument of type 'number is not assignable to parameter of type 'string'. (esto sucede porque especificamos previamente (name: string) )
+
+// FORMA Nª1
+//function saludar({ name, age }: { name: string, age: number }) {
+//    console.log(`Hola ${name}, tienes ${age} años`)
+//}
+
+//saludar({ name: 'Rick', age: 33 })
+
+// FORMA Nª2
+//function saludar2(persona: { name: string, age: number }) {
+//    const { name, age } = persona
+//    console.log(`Hola ${name}, tienes ${age} años`)
+//}
+
+//saludar2({ name: 'Rick', age: 33 })
